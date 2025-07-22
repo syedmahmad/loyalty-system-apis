1ad736ed10b40247f18b30d4bdb323758316d93a
syedmahmad <devm.ahmad@gmail.com>
2025-07-22 18:05:56 +0500

    hot fix

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,18 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const allowedOrigins = process.env.ALLOWED_CORS_URLS?.split(',') || [];
+
   app.enableCors({
-    origin: process.env.ALLOWED_CORS_URL, // ✅ your frontend URL
+    // origin: process.env.ALLOWED_CORS_URL, // ✅ your frontend URL
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     credentials: true,
   });
   await app.listen(3000);
