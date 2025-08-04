367406b3f076bb64db8572e5b5d25dffe36da28b
deepakRampwin <deepak.mahato@rampwin.com>
2025-08-04 10:07:13 +0530

    single coupon and analytics

--- src/analytics/analytics.module.ts
+++ src/analytics/analytics.module.ts
@@ -5,9 +5,12 @@ import { WalletTransaction } from 'src/wallet/entities/wallet-transaction.entity
 import { WalletOrder } from 'src/wallet/entities/wallet-order.entity';
 import { LoyaltyAnalyticsController } from './analytics/analytics.controller';
 import { LoyaltyAnalyticsService } from './analytics/analytics.service';
+import { Coupon } from 'src/coupons/entities/coupon.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Wallet, WalletTransaction, WalletOrder])],
+  imports: [
+    TypeOrmModule.forFeature([Wallet, WalletTransaction, WalletOrder, Coupon]),
+  ],
   controllers: [LoyaltyAnalyticsController],
   providers: [LoyaltyAnalyticsService],
 })
