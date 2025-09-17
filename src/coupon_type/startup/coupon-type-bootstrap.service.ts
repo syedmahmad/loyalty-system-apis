19f7de2b53b40a7a5cf8f6e0c7e4802bde44dee9
syedmahmad <devm.ahmad@gmail.com>
2025-09-17 18:12:36 +0500

    hot fix

--- src/coupon_type/startup/coupon-type-bootstrap.service.ts
+++ src/coupon_type/startup/coupon-type-bootstrap.service.ts
@@ -1,7 +1,7 @@
 // coupon_type/startup/coupon-type-bootstrap.service.ts
 import { Injectable, OnApplicationBootstrap } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { In, Repository } from 'typeorm';
+import { Repository } from 'typeorm';
 import { CouponType } from '../entities/coupon_type.entity';
 import { Tenant } from 'src/tenants/entities/tenant.entity';
 
@@ -15,77 +15,72 @@ export class CouponTypeBootstrapService implements OnApplicationBootstrap {
   ) {}
 
   async onApplicationBootstrap() {
-    const couponTypes = [
-      'VEHICLE_SPECIFIC',
-      'USER_SPECIFIC',
-      'PRODUCT_SPECIFIC',
-      'GEO_TARGETED',
-      'SERVICE_BASED',
-      'BIRTHDAY',
-      'REFERRAL',
-      'TIER_BASED',
-      'CASHBACK',
-      'DISCOUNT',
-    ];
-
-    const conditions = {
-      VEHICLE_SPECIFIC: [
-        { name: 'fuel_type' },
-        { name: 'color' },
-        { name: 'Cylinder' },
-        { name: 'body_type' },
-      ],
-      USER_SPECIFIC: [
-        { name: 'EMAIL_DOMAIN' },
-        { name: 'EMAIL' },
-        { name: 'PHONE_NUMBER' },
-        { name: 'NOT_APPLICABLE' },
-        { name: 'VALIDATE_OTP' },
-      ],
-      PRODUCT_SPECIFIC: [{ name: 'applicableProductIds' }],
-      GEO_TARGETED: [{ name: 'allowedCities' }, { name: 'allowedZipCodes' }],
-      SERVICE_BASED: [{ name: '' }],
-      BIRTHDAY: [
-        { name: 'minimumPurchase' },
-        { name: 'userMustVerifyBirthday' },
-        { name: 'validOnCategories' },
-      ],
-      REFERRAL: [
-        { name: 'referredUserFirstPurchaseMinAmount' },
-        { name: 'referredUserSignupRequired' },
-        { name: 'referrerReward' },
-      ],
-      TIER_BASED: [{ name: 'discountPercentage' }],
-      CASHBACK: [{ name: 'minPurchaseAmount' }],
-      DISCOUNT: [{ name: 'Total Purchase Amount' }],
-    };
-
-    const existing = await this.couponTypeRepo.findOne({
-      where: {
-        coupon_type: In(couponTypes),
-      },
-    });
-
-    if (!existing) {
-      const existingTenant = await this.tenantRepo.find();
-      const tenantId = existingTenant[0].id;
-
-      const now = new Date();
-
-      const defaultCouponTypes = couponTypes.map((type) =>
-        this.couponTypeRepo.create({
-          tenant_id: tenantId,
-          coupon_type: type,
-          is_active: 1,
-          conditions: conditions[type] || [],
-          created_at: now,
-          updated_at: now,
-        }),
-      );
-      await this.couponTypeRepo.save(defaultCouponTypes);
-      console.log('✅ Default Coupon Type created');
-    } else {
-      console.log('ℹ️ Default Coupon Type already exists');
-    }
+    // const couponTypes = [
+    //   'VEHICLE_SPECIFIC',
+    //   'USER_SPECIFIC',
+    //   'PRODUCT_SPECIFIC',
+    //   'GEO_TARGETED',
+    //   'SERVICE_BASED',
+    //   'BIRTHDAY',
+    //   'REFERRAL',
+    //   'TIER_BASED',
+    //   'CASHBACK',
+    //   'DISCOUNT',
+    // ];
+    // const conditions = {
+    //   VEHICLE_SPECIFIC: [
+    //     { name: 'fuel_type' },
+    //     { name: 'color' },
+    //     { name: 'Cylinder' },
+    //     { name: 'body_type' },
+    //   ],
+    //   USER_SPECIFIC: [
+    //     { name: 'EMAIL_DOMAIN' },
+    //     { name: 'EMAIL' },
+    //     { name: 'PHONE_NUMBER' },
+    //     { name: 'NOT_APPLICABLE' },
+    //     { name: 'VALIDATE_OTP' },
+    //   ],
+    //   PRODUCT_SPECIFIC: [{ name: 'applicableProductIds' }],
+    //   GEO_TARGETED: [{ name: 'allowedCities' }, { name: 'allowedZipCodes' }],
+    //   SERVICE_BASED: [{ name: '' }],
+    //   BIRTHDAY: [
+    //     { name: 'minimumPurchase' },
+    //     { name: 'userMustVerifyBirthday' },
+    //     { name: 'validOnCategories' },
+    //   ],
+    //   REFERRAL: [
+    //     { name: 'referredUserFirstPurchaseMinAmount' },
+    //     { name: 'referredUserSignupRequired' },
+    //     { name: 'referrerReward' },
+    //   ],
+    //   TIER_BASED: [{ name: 'discountPercentage' }],
+    //   CASHBACK: [{ name: 'minPurchaseAmount' }],
+    //   DISCOUNT: [{ name: 'Total Purchase Amount' }],
+    // };
+    // const existing = await this.couponTypeRepo.findOne({
+    //   where: {
+    //     coupon_type: In(couponTypes),
+    //   },
+    // });
+    // if (!existing) {
+    //   const existingTenant = await this.tenantRepo.find();
+    //   const tenantId = existingTenant[0].id;
+    //   const now = new Date();
+    //   const defaultCouponTypes = couponTypes.map((type) =>
+    //     this.couponTypeRepo.create({
+    //       tenant_id: tenantId,
+    //       coupon_type: type,
+    //       is_active: 1,
+    //       conditions: conditions[type] || [],
+    //       created_at: now,
+    //       updated_at: now,
+    //     }),
+    //   );
+    //   await this.couponTypeRepo.save(defaultCouponTypes);
+    //   console.log('✅ Default Coupon Type created');
+    // } else {
+    //   console.log('ℹ️ Default Coupon Type already exists');
+    // }
   }
 }
