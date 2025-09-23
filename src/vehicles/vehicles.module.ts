5e3df8eb8cfa9e652b0500c309d88140571a4a30
syedmahmad <devm.ahmad@gmail.com>
2025-09-23 20:21:46 +0500

    hot fix

--- src/vehicles/vehicles.module.ts
+++ src/vehicles/vehicles.module.ts
@@ -6,10 +6,11 @@ import { VehiclesService } from './vehicles/vehicles.service';
 import { Customer } from 'src/customers/entities/customer.entity';
 import { MakeEntity } from 'src/make/entities/make.entity';
 import { ModelEntity } from 'src/model/entities/model.entity';
+import { Log } from 'src/logs/entities/log.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Vehicle, Customer, MakeEntity, ModelEntity]),
+    TypeOrmModule.forFeature([Vehicle, Customer, MakeEntity, ModelEntity, Log]),
   ],
   controllers: [VehiclesController],
   providers: [VehiclesService],
