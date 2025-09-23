b747c03f6d0aae4e81f23e4d750e148181471243
syedmahmad <devm.ahmad@gmail.com>
2025-09-23 20:32:11 +0500

    hot fix

--- src/petromin-it/preferences/dto/update-preferences.dto.ts
+++ src/petromin-it/preferences/dto/update-preferences.dto.ts
@@ -1,9 +1,17 @@
+import { IsOptional } from 'class-validator';
+
 export class UpdatePreferencesDto {
+  @IsOptional()
   email_notification?: number; // 0 | 1
+  @IsOptional()
   whatsapp_notification?: number; // 0 | 1
+  @IsOptional()
   sms_notification?: number; // 0 | 1
+  @IsOptional()
   push_notification?: number; // 0 | 1
+  @IsOptional()
   location_access?: number; // 0 | 1
+  @IsOptional()
   biometric?: number; // 0 | 1
 }
 
