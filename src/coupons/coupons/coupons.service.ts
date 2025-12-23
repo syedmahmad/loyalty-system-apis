9648fb424957a65e383757038ae528619c580061
syedmahmad <devm.ahmad@gmail.com>
2025-12-23 12:48:17 +0500

    fixed coupons syncing

--- src/coupons/coupons/coupons.service.ts
+++ src/coupons/coupons/coupons.service.ts
@@ -2358,7 +2358,7 @@ export class CouponsService {
         });
         if (coupon) {
           successCoupons.push(eachCoupon);
-          const phoneNumber = eachCoupon.customer_phone_no;
+          const phoneNumber = eachCoupon.customer_phone;
           const customer = await this.findCustomerByFullPhone(phoneNumber);
           if (customer) {
             const couponUsageObj = {
